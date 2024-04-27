5b7996b53f3cf082020f10000e4a107c1f52bef1
Anton-Osip <osipchika@gmail.com>
2024-04-27 09:32:37 +0300

    feat: add images and creat svg sprite

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import './App.css';
-import styled from 'styled-components';
-
+import styled from 'styled-components'
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <Title>Welcome to IT-INCUBATOR</Title>
-        </div>
-    );
+	return (
+		<div className='App'>
+			<Title>Welcome to IT-INCUBATOR</Title>
+		</div>
+	)
 }
 
-export default App;
+export default App
 
 const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+	font-size: 1.5em;
+	text-align: center;
+	color: #e91e63;
+`
