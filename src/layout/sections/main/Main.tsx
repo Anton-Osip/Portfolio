f485bc1fffc648b3c4c40232c710bba9fa6af921
Anton-Osip <osipchika@gmail.com>
2024-05-03 19:29:16 +0300

    feat: add styles for section main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,31 +1,70 @@
 import styled from 'styled-components'
+import abstract from '../../../assets/image/abstract.svg'
 import photo from '../../../assets/image/photo.png'
+import { Container } from '../../../components/Container'
 import { FlexWrapper } from '../../../components/FlexWrapper'
+import { theme } from '../../../styles/Theme'
+
 export function Main() {
 	return (
 		<StyledMain>
-			<FlexWrapper align='center' justify='space-between'>
-				<MainTitle>
-					Hi 👋,
-					<br /> My name is <br />
-					<span>Pavan MG</span> <br />I build things for web
-				</MainTitle>
-				<Photo src={photo} alt='photo' />
-			</FlexWrapper>
+			<Container>
+				<FlexWrapper align='center' justify='space-between'>
+					<MainTitle>
+						Hi 👋,
+						<br /> My name is <br />
+						<span>Pavan MG</span> <br />I build things for web
+					</MainTitle>
+					<PhotoWrapper>
+						<Photo src={photo} alt='photo' />
+					</PhotoWrapper>
+				</FlexWrapper>
+			</Container>
 		</StyledMain>
 	)
 }
-
-const MainTitle = styled.h1``
-
-const StyledMain = styled.main`
+const StyledMain = styled.section`
 	min-height: 100lvh;
+	display: flex;
+	background-image: url(${abstract});
+	background-repeat: no-repeat;
+	background-position: right center;
 `
 
+const MainTitle = styled.h1`
+	font-size: 58px;
+	font-weight: 700;
+	line-height: 70px;
+	letter-spacing: -1px;
+	color: ${theme.colors.mainTitleColor};
+	span {
+		background: linear-gradient(45deg, rgb(19, 176, 245), rgb(231, 15, 170));
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+		color: rgb(217, 217, 217);
+	}
+`
+
+const PhotoWrapper = styled.div`
+	position: relative;
+
+	&::before {
+		z-index: 5;
+		content: '';
+		position: absolute;
+		top: -9px;
+		left: -9px;
+		width: 367px;
+		height: 367px;
+		border-radius: 50%;
+		background: linear-gradient(rgb(231, 15, 170), rgb(19, 176, 245));
+	}
+`
 const Photo = styled.img`
 	width: 349px;
 	height: 349px;
 	object-fit: cover;
 	border-radius: 50%;
-	border: 9px solid #9198e5;
+	position: relative;
+	z-index: 5;
 `
