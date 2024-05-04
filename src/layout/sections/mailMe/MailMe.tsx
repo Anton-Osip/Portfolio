3d8d8971a7e773131fb7ce7a134f36b6f8719358
Anton-Osip <osipchika@gmail.com>
2024-05-04 16:49:29 +0300

    feat: add styles for section Contacts

--- src/layout/sections/mailMe/MailMe.tsx
+++ src/layout/sections/mailMe/MailMe.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { theme } from '../../../styles/Theme'
 import { Footer } from '../../footer/Footer'
 
 export function MailMe() {
@@ -14,11 +15,30 @@ export function MailMe() {
 }
 
 const StyledMailMe = styled.section`
-	min-height: 100lvh;
+	min-height: calc(100lvh - 157px);
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 `
-const StyledDescription = styled.p``
-const StyledLink = styled.a``
+const StyledDescription = styled.p`
+	color: ${theme.colors.fontColor};
+	font-family: DM Sans;
+	font-size: 58px;
+	font-weight: 700;
+	line-height: 70px;
+	letter-spacing: -1px;
+	text-align: center;
+`
+const StyledLink = styled.a`
+	background: linear-gradient(90deg, rgb(19, 176, 245), rgb(231, 15, 170));
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
+	background-clip: text;
+	font-family: DM Sans;
+	font-size: 58px;
+	font-weight: 700;
+	line-height: 70px;
+	letter-spacing: -1px;
+	text-align: center;
+`
